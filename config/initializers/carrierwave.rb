cae4eb346fb29f4b71f84a1728fd74c06e890c4a
Shinnosuke Yoshizawa <pokopanic19@gmail.com>
2017-11-11 17:28:53 +0900

    change storage to fog from file

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,10 +12,10 @@ CarrierWave.configure do |config|
 
   case Rails.env
   when 'production'
-    config.fog_directory = '本番環境用のバケット名'
-    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/本番環境用のバケット名'
+    config.fog_directory = 'shxn-achieve-production'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/shxn-achieve-production'
   when 'development'
-    config.fog_directory = '開発環境用のバケット名'
-    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/開発環境用のバケット名'
+    config.fog_directory = 'shxn-achieve-development'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/shxn-achieve-development'
   end
 end
